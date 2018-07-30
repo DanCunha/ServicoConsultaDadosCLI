59bb0a861e9865cc8456f8a3a3d25c272a2c2382
Daniel Costa Cunha <daniel.cunha@ITBAM.ORG.BR>
2018-07-30 17:48:00 -0400

    Commit Inicial

---
+++ ServicoConsultaDados/ClienteHttp/ClienteHttp.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+
+namespace ServicoConsultaDados.ClienteHttp
+{
+    public class ClienteHttp
+    {
+        string tokenAcesso = "912c5f459b7ebebbb14be297d50c60a9";
+        string cnpj = "15828398000126";
+
+        public async void GetAllProdutos()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri("http://localhost:50306/");
+                using (var response = await client.GetAsync("api/values"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //var dadodClientes = await response.Content.ReadAsStringAsync();
+                        //RootObject obj = JsonConvert.DeserializeObject<RootObject>(dadodClientes);
+
+                        //Console.WriteLine("Produto acessado e exibido.  Tecle algo para incluir um novo produto.");
+                        //Console.ReadKey();
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ServicoConsultaDados/Service1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicoConsultaDados
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");            
+            // Set up a timer to trigger every minute.  
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 60 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        private int eventId = 1;
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.  
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+            ClienteHttp.ClienteHttp c = new ClienteHttp.ClienteHttp();
+            c.GetAllProdutos();
+
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In onStop.");
+        }
+    }
+}
